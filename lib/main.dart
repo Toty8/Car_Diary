16dc65364071f71ab500d7c492b6e584e18147f7
Toty8 <80042351+Toty8@users.noreply.github.com>
2025-04-21 15:46:47 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( CarDiary() );
+}
+
+class CarDiary extends StatelessWidget {
+  const CarDiary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
